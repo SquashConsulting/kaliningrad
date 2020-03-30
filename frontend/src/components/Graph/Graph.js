c04f3e9e5119a5eeaa187a2ce3688708a85d1a46
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-03-30 05:27:09 +0400

    Create a D3 Graph component

---
+++ frontend/src/components/Graph/Graph.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import { Graph as D3Graph } from 'react-d3-graph';
+
+import { UIContext } from 'contexts/ui';
+import { GraphContext } from 'contexts/graph';
+
+import config from './config';
+
+const Graph = () => {
+  const { data } = useContext(GraphContext);
+  const { setSelected } = useContext(UIContext);
+
+  return (
+    <D3Graph
+      id="d3-graph"
+      {...{
+        data,
+        config,
+        onClickLink: setSelected('link'),
+        onClickNode: setSelected('node'),
+      }}
+    />
+  );
+};
+
+export default Graph;
