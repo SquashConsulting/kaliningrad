948501b778e8031ade9ef9bc7fa5731912b6189a
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2020-04-08 19:51:49 +0400

    [Frontend]: Make filename required in graph download dialog

--- frontend/src/components/Dialogs/Save.js
+++ frontend/src/components/Dialogs/Save.js
@@ -14,14 +14,18 @@ export const TYPE = 'save';
 
 export const Dialog = () => {
   const [name, setName] = useState('');
+  const [error, setError] = useState(false);
   const { data } = useContext(GraphContext);
   const { setDialogs } = useContext(UIContext);
 
   const handleNameChange = ({ target: { value } }) => {
+    setError(false);
     setName(value);
   };
 
   const handler = () => {
+    if (!name.length) return setError(true);
+
     const blob = new Blob([JSON.stringify(data)], {
       type: 'application/json',
     });
@@ -42,11 +46,13 @@ export const Dialog = () => {
           <b>only</b>.
         </DialogContentText>
         <TextField
+          required
           autoFocus
           fullWidth
           value={name}
+          error={error}
           onChange={handleNameChange}
-          label="File Name (without the extension)"
+          label="File Name (we will append .json)"
         />
       </DialogContent>
     </DialogCore>
